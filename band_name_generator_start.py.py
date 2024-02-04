9a02666690b0f02a77ef4e5b860a955390b59e4e
yongjun kwon <rnjsdld12@naver.com>
2024-02-04 22:58:05 +0900

    one day

---
+++ band_name_generator_start.py.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+city = input("당신이 사는 곳은 어디인가요?\n")
+pet = input("당신이 키우는 애완동물의 이름은 무엇인가요?\n")
+Ask = city + ' ' + pet
+print("Your band name could be" + ' ' + Ask)
