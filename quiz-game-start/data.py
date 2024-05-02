246b3c5c19ab572910cca5ab45f4fe82efe47ef0
yong jun <156164376+dydwns97@users.noreply.github.com>
2024-05-02 22:15:16 +0900

    day 17 퀴즈게임 프로젝트

---
+++ quiz-game-start/data.py
@@ -0,0 +1,15 @@
+question_data = [
+    {"text": "달팽이'의 blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "인간 폐의 총 표면적은 축구장 크기와 같다.", "answer": "True"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "미국 서부 버지니아에서 차량 사고로 동물을 우연히 치면 집으로 가져가서 먹을 수 있다.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, "
+             "you are entitled to a state funeral.", "answer": "False"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+]
